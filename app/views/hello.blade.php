83fed9f63d89e3b911cbc0c4aa929bd7f6e0f9e9
Jyrone Parker <jyrone.parker@gmail.com>
2014-11-17 22:54:39 +0000

    Fixed link issues and added ajax function for deletes

--- app/views/hello.blade.php
+++ app/views/hello.blade.php
@@ -16,7 +16,7 @@
 		<h1>Make Your Requests Known</h1>
 		<div class="song_request">
 		<h2>Song Request</h2>
-		<form action="http://localhost/godj_api/public/index.php/api/v1/songs" method="POST">
+		<form action="/api/v1/songs" method="POST">
   <input type="text" name="requestor_name" placeholder="Your Name"><br>
   <input type="text" name="title" placeholder="Song Title"><br>
   <input type="text" name="artist" placeholder="Artist"><br>
@@ -26,7 +26,7 @@
 </div>
 <div class="mood_request">
 <h2>Mood Request</h2>
-<form action="http://localhost/godj_api/public/index.php/api/v1/moods" method="POST">
+<form action="/api/v1/moods" method="POST">
   <input type="text" name="requestor_name" placeholder="Your Name"><br>
   <input type="text" name="title" placeholder="What Mood Are You Looking For?"><br>
   <input type="text" name="dj_id" placeholder="DJ Name"><br>
--- app/views/profile.blade.php
+++ app/views/profile.blade.php
@@ -6,7 +6,31 @@ ADD GOOGLE VISUALS HERE
 
 @section('content')
 <script>
+function deleteSong(id) {
+    if (confirm('Delete this song request?')) {
+        $.ajax({
+            type: "DELETE",
+            url: '/api/v1/songs/' + id, //resource
+            success: function(affectedRows) {
+                //if something was deleted, we redirect the user to the users page, and automaticaly tne user that he deleted will disapear
+		location.reload();         
+   }
+        });
+    }
+}
 
+function deleteMood(id) {
+    if (confirm('Delete this mood request?')) {
+        $.ajax({
+            type: "DELETE",
+            url: '/api/v1/moods/' + id, //resource
+            success: function(affectedRows) {
+                //if something was deleted, we redirect the user to the users page, and automaticaly tne user that he deleted will disapear
+                location.reload();         
+   }
+        });
+    }
+}
 </script>
 <h1>DJ Profile</h1>
 <h3> Song Requests</h3>
