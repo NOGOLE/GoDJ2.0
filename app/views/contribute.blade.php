e8228da035446fd852629f504a5ff861e27be246
Jyrone Parker <jyrone.parker@gmail.com>
2014-11-19 17:53:36 +0000

    Removed class and id from paragragh, surrounded the existing code in a div and added class and id to the div

--- app/views/contribute.blade.php
+++ app/views/contribute.blade.php
@@ -10,16 +10,20 @@
 color: #000;
 }
 </style>
-<p class="jumbotron" id="about">You can help contribute to GoDJ in 3 major ways:
+<div class="jumbotron" id="about">
+<p>
+You can contribute to GoDJ in 3 major ways:
 <ul>
 <li>Monetarily</li>
 <li>Socially</li>
 <li>Programmatically</li>
 </ul>
 Servers cost money. By contributing monetarily to our Paypal account, you can help us 
-maintain basic costs associated with running an app. Another way to help out, is by
-giving us a social shout out on Twitter, Facebook, Pinterest, etc. Getting the word
+maintain basic costs associated with running an app.[TODO: INSERT PAYPAL BUTTON HERE] Another way to help out, is by
+giving us a social shout out on Twitter, Facebook, Pinterest, etc.[TODO: INSERT SOCIAL ICONS HERE] Getting the word
 out is just as important as having a good app! Lastly if you want to help the app grow, then fork it
-off of Github, make some cool features, then put it back in the repo!.
+off of Github, make some cool features, then put it back in the repo!.[TODO: INSERT A FORK ME BUTTON  HERE]
+
 </p>
+</div>
 @stop
