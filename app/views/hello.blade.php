69d2e09bd89a6cafe4ce8efb2a21a492cdc957a3
Jyrone Parker <jyrone.parker@gmail.com>
2014-12-15 04:10:16 +0000

    Fixed issue preventing users from making requests

--- app/views/hello.blade.php
+++ app/views/hello.blade.php
@@ -22,7 +22,7 @@
   <input class="request_form_field row col-xs-" type="text" ng-model="song_title" placeholder="Song Title"><br><br>
   <input class="request_form_field row col-xs-" type="text" ng-model="song_artist" placeholder="Artist"><br><br>
   <input class="request_form_field row col-xs-" type="text" ng-model="song_dj_id" placeholder="DJ Name"><br><br>
-  <input  ng-click="submitSong() row col-xs-" type="submit"  value="Submit Request" class="btn btn-primary">
+  <input  ng-click="submitSong() " type="submit"  value="Submit Request" class="btn btn-primary">
 </form>
 </div>
 <div class="mood_request">
@@ -31,7 +31,7 @@
   <input class="request_form_field row col-xs-" type="text" ng-model="mood_requestor_name" placeholder="Your Name"><br><br>
   <input class="request_form_field row col-xs-" type="text" ng-model="mood_title" placeholder="What Mood?"><br><br>
   <input class="request_form_field row col-xs-" type="text" ng-model="mood_dj_id" placeholder="DJ Name"><br><br>
-  <input  ng-click="submitMood() row col-xs-" type="submit" value="Submit Request" class="btn btn-primary">
+  <input  ng-click="submitMood() " type="submit" value="Submit Request" class="btn btn-primary">
 </form>	
 		</div>
 </div>	
