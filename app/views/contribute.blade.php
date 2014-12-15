bb61b6564dd1ba394535baf8024e94e9e6f8ea2f
Jyrone Parker <jyrone.parker@gmail.com>
2014-12-15 04:01:59 +0000

    Fixed programmatically message to appropriate spot

--- app/views/contribute.blade.php
+++ app/views/contribute.blade.php
@@ -59,11 +59,11 @@ We need help getting the word out about this app and NOGOLE in general! Give us 
 <div class="panel panel-success">
   <div class="panel-heading">
     <h3 class="panel-title">Programmatically</h3>
+  </div>
+  <div class="panel-body">
 	Currently we only have  one programmer, yet we have a plethora of ideas for this app! Please feel free to fork us from Github
 <a href="https://github.com/NOGOLE/GoDJ2.0"><img style="position: absolute; top: 0; left: 0; border: 0;" src="https://camo.githubusercontent.com/567c3a48d796e2fc06ea80409cc9dd82bf714434/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f6461726b626c75655f3132313632312e706e67" 
 alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_left_darkblue_121621.png"></a>
-  </div>
-  <div class="panel-body">
 
   </div>
 
