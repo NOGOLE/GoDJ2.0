96b3154c24d1adea741010e856cea0bf7186ff14
Jyrone Parker <jyrone_parker@apple.com>
2015-02-22 00:49:56 -0800

    error fix

--- public/js/app.js
+++ public/js/app.js
@@ -5,7 +5,7 @@ app.config(['$routeProvider',
   function($routeProvider) {
     $routeProvider.
       when('/home', {
-        templateUrl: '#/partials/home.html',
+        templateUrl: './partials/home.html',
         //controller: 'PhoneListCtrl'
       })
 		}]);
