fc708119fbfb3597e871a4fbba09617d9545b93a
Jyrone Parker <jyrone.parker@gmail.com>
2015-05-17 16:31:01 -0700

    fixed tags

--- app/views/registration.blade.php
+++ app/views/registration.blade.php
@@ -8,30 +8,30 @@
 <div class="jumbotron how-to">
 If you would like to try GoDJ for yourself, then please create an account using the form below!
 </div>
-{{ Form::open(array('url' => 'api/v1/createuser')) }}
+<% Form::open(array('url' => 'api/v1/createuser')) %>
                 <h1>Create A New Account</h1>
 
                 <!-- if there are login errors, show them here -->
                 <p>
-                        {{ $errors->first('email') }}
-                        {{ $errors->first('password') }}
+                        <% $errors->first('email') %>
+                        <% $errors->first('password') %>
                 </p>
 
                <p>
-		{{Form::text('username', Input::old('username'), array('class'=>'request_form_field','id'=>'username','placeholder' => 'DJ name w/o the word DJ')) }}
+		<%Form::text('username', Input::old('username'), array('class'=>'request_form_field','id'=>'username','placeholder' => 'DJ name w/o the word DJ')) %>
 		 <p>
-                        {{ Form::text('email', Input::old('email'), array('id'=>'email','class'=>'request_form_field', 'placeholder' => 'Email')) }}
+                        <% Form::text('email', Input::old('email'), array('id'=>'email','class'=>'request_form_field', 'placeholder' => 'Email')) %>
                 </p>
 
 	<p>
-                        {{ Form::password('password', array('id'=>'password','class'=>'request_form_field', 'placeholder' => 'Password')) }}
+                        <% Form::password('password', array('id'=>'password','class'=>'request_form_field', 'placeholder' => 'Password')) %>
                 </p>
 
                 <p>
-                        {{ Form::password('password', array('class'=>'request_form_field','placeholder' => 'Repeat Password')) }}
+                        <% Form::password('password', array('class'=>'request_form_field','placeholder' => 'Repeat Password')) %>
                 </p>
 
-                <p>{{ Form::submit('Register!', array('class' => 'btn btn-primary')) }}</p>
-        {{ Form::close() }}
+                <p><% Form::submit('Register!', array('class' => 'btn btn-primary')) %></p>
+        <% Form::close() %>
 
 @stop
