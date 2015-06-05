3c70ff05e1fefcc334274efa6420b8d8810a58a4
Jyrone Parker <jyrone.parker@gmail.com>
2015-06-05 16:56:40 +0000

    Modified Registration page to reflect database changes, added bio textarea and file upload

--- app/views/registration.blade.php
+++ app/views/registration.blade.php
@@ -31,6 +31,15 @@ If you would like to try GoDJ for yourself, then please create an account using 
                         <% Form::password('password', array('class'=>'request_form_field form-control','placeholder' => 'Repeat Password')) %>
                 </p>
 
+                <p>
+                        <% Form::textArea('bio', Input::old('bio'),array('class'=>'request_form_field form-control','placeholder' => 'Your Bio')) %>
+                </p>
+
+                <p>
+                        <% Form::label('Profile Pic','Profile Pic',array('class'=>'request_form_field form-control'))%>
+                        <% Form::file('profile_pic', array('class'=>'request_form_field form-control','text' => 'Your Profile Picture')) %>
+                </p>
+
                 <p><% Form::submit('Register!', array('class' => 'btn btn-success')) %></p>
         <% Form::close() %>
 
